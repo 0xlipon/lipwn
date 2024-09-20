f6fffd6c7dfd1b854bbfbe67afaeea73db08a413
Md. Torikul Islam Lipon <124679777+0xlipon@users.noreply.github.com>
2024-09-20 20:30:59 +0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+google-chrome --version
+rm -rf google-chrome-stable_current_amd64.deb
