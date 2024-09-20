5c6f24572b337cd68229990fec9ca4dcc40cd5bb
Md. Torikul Islam Lipon <124679777+0xlipon@users.noreply.github.com>
2024-09-21 00:14:39 +0600

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb
-google-chrome --version
-rm -rf google-chrome-stable_current_amd64.deb
